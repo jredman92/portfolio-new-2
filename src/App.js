a7e641a6b2e2f5a5826e83eccbcdb3c2894293c8
jaymie <jredman92@gmail.com>
2023-10-13 19:54:37 -0700

    fade-in nav working on homepage letters

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
+import { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import j from "./images/name-logo.jpg";
 
 function App() {
+   useEffect(() => {
+      const parallax_el = document.querySelectorAll(".parallax");
+      let xValue = 0,
+         yValue = 0;
+      let rotateDegree = 0;
+
+      function update(cursorPosition) {
+         parallax_el.forEach((el) => {
+            let speedx = el.dataset.speedx;
+            let speedy = el.dataset.speedy;
+            let speedz = el.dataset.speedz;
+            let rotateSpeed = el.dataset.rotation;
+
+            let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+            let zValue = (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+
+            el.style.transform = `perspective(2300px) translateZ(${zValue * speedz}px) rotateY(${
+               rotateDegree * rotateSpeed
+            }deg) translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`;
+         });
+      }
+
+      window.addEventListener("mousemove", (e) => {
+         xValue = e.clientX - window.innerWidth / 2;
+         yValue = e.clientY - window.innerHeight / 2;
+
+         rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+
+         update(e.clientX);
+      });
+   }, []);
+
    return (
       <>
          <Navbar />
 
-         <div
-            className="j"
-            style={{ height: "100vh" }}
-         >
-            <img
-               height="400"
-               width="300"
-               src={j}
-               alt="logo"
-            />
+         <div>
+            <p className="text-6xl font-black flex pt-[460px] pl-[830px] text-blue-400">HI</p>
+            <div
+               className="j"
+               style={{ height: "100vh" }}
+            >
+               <img
+                  height="400"
+                  width="300"
+                  src="img/name-logo.jpg"
+                  data-speedx="0.025"
+                  data-speedy="0.055"
+                  data-speedz="0.015"
+                  data-rotation="0.02"
+                  data-distance="1700"
+                  alt=""
+                  className="parallax j"
+               />
+            </div>
          </div>
       </>
    );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import { gsap } from "gsap";
+import { useEffect } from "react";
 
 const Navbar = () => {
+   useEffect(() => {
+      const nav = document.querySelector(".nav");
+
+      const tl = gsap.timeline();
+
+      tl.set(nav, { opacity: 0 });
+
+      tl.to(nav, { opacity: 1, duration: 3, delay: 2, ease: "power1.out" });
+   }, []);
+
    return (
-      <nav className="nav">
-         <a
-            href="/"
-            className="text-xl"
-         >
-            Site Name
-         </a>
-         <ul>
+      <nav className="nav flex justify-end items-center bg-white text-black pr-96 pt-6">
+         <ul className="flex gap-12">
+            <li>
+               <a href="/">Home</a>
+            </li>
             <li>
                <a href="/work">Work</a>
             </li>
